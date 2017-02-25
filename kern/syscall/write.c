031ac18615057803f4f5589ccbe02408c45e00fa
Shubham Lagwankar <shubhu105@gmail.com>
2017-02-24 21:20:36 -0500

    write: check for partial writes

--- kern/syscall/write.c
+++ kern/syscall/write.c
@@ -15,38 +15,38 @@
 ssize_t
 sys_write(int fd, const_userptr_t user_buf, size_t buflen)
 {
-	int result, max_fds;
-	char *kbuffer;
-	struct uio uio;
-	struct iovec iov;
-	struct vnode *vnode;
-	struct file_entry *fentry;
+    int result, max_fds;
+    char *kbuffer;
+    struct uio uio;
+    struct iovec iov;
+    struct vnode *vnode;
+    struct file_entry *fentry;
 
-	/* check file descriptor and buffer pointer */
-	if (fd <= 0) {
-		return EBADF;
-	}
-	if (user_buf == NULL) {
-		return EFAULT;
-	}
+    /* check file descriptor and buffer pointer */
+    if (fd <= 0) {
+        return EBADF;
+    }
+    if (user_buf == NULL) {
+        return EFAULT;
+    }
 
-	/* initialize buffer */
-	kbuffer = kmalloc(buflen * sizeof(char));
-	if (kbuffer == NULL) {
-		return ENOSPC;
-	}
+    /* initialize buffer */
+    kbuffer = kmalloc(buflen * sizeof(char));
+    if (kbuffer == NULL) {
+        return ENOSPC;
+    }
 
-	/* load data into buffer */
-	result = copyin(user_buf, kbuffer, buflen);
-	if (result) {
-		kfree(kbuffer);
-		return result;
-	}
+    /* load data into buffer */
+    result = copyin(user_buf, kbuffer, buflen);
+    if (result) {
+        kfree(kbuffer);
+        return result;
+    }
 
-	/* create a uio for vop_write */
-	uio_kinit(&iov, &uio, kbuffer, buflen, 0, UIO_WRITE);
+    /* create a uio for vop_write */
+    uio_kinit(&iov, &uio, kbuffer, buflen, 0, UIO_WRITE);
 
-	/* check if fd has an entry in the filetable */
+    /* check if fd has an entry in the filetable */
     max_fds = file_entryarray_num(curproc->p_filetable);
     if (max_fds < 3 || fd >= max_fds) {
         kfree(kbuffer);
@@ -54,20 +54,29 @@ sys_write(int fd, const_userptr_t user_buf, size_t buflen)
     }
 
     /* get fd's entry from filetable */
-	fentry = file_entryarray_get(curproc->p_filetable, fd);
-	if (fentry == NULL || fentry->f_mode == O_RDONLY) {
-		kfree(kbuffer);
-		return EBADF;
-	}
+    fentry = file_entryarray_get(curproc->p_filetable, fd);
+    if (fentry == NULL || fentry->f_mode == O_RDONLY) {
+        kfree(kbuffer);
+        return EBADF;
+    }
 
-	vnode = fentry->f_node;
+    vnode = fentry->f_node;
 
-	/* do the write */
-	result = VOP_WRITE(vnode, &uio);
-	if (result) {
-		kfree(kbuffer);
-		return result;
-	}
+    /* do the write */
+write:
+    result = VOP_WRITE(vnode, &uio);
+    if (result) {
+        kfree(kbuffer);
+        return result;
+    }
 
-	return buflen;
+    /* make sure we wrote everything */
+    if (buflen != uio.uio_offset) {
+        goto write;
+    }
+
+    /* update offset */
+    fentry->f_offset = buflen;
+
+    return buflen;
 }
