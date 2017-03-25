f6abbf363cf07edb97c5012ecf3636897d91201c
Shubham Lagwankar <shubhu105@gmail.com>
2017-03-25 11:22:36 -0400

    vm skeleton, compiles but does not boot

---
+++ kern/vm/vm.c
@@ -0,0 +1,41 @@
+#include <types.h>
+#include <vm.h>
+
+void
+vm_bootstrap(void)
+{
+
+}
+
+int
+vm_fault(int faulttype, vaddr_t faultaddress)
+{
+    (void)faulttype;
+    (void)faultaddress;
+    return 0;
+}
+
+vaddr_t
+alloc_kpages(unsigned npages)
+{
+    (void)npages;
+    return 1;
+}
+
+void
+free_kpages(vaddr_t addr)
+{
+    (void)addr;
+}
+
+unsigned int
+coremap_used_bytes(void)
+{
+    return 1;
+}
+
+void
+vm_tlbshootdown(const struct tlbshootdown *tlbshootdown)
+{
+    (void)tlbshootdown;
+}
