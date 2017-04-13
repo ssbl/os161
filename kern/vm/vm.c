954bb56171dba7713d617e5e4df3477433c10aa1
Shubham Lagwankar <shubhu105@gmail.com>
2017-04-13 07:37:42 -0400

    dumbvm-style vm_fault

--- kern/vm/vm.c
+++ kern/vm/vm.c
@@ -1,5 +1,12 @@
 #include <types.h>
+#include <lib.h>
+#include <spl.h>
+#include <kern/errno.h>
+#include <current.h>
+#include <proc.h>
 #include <vm.h>
+#include <mips/tlb.h>
+#include <addrspace.h>
 #include <coremap.h>
 
 void
@@ -11,9 +18,78 @@ vm_bootstrap(void)
 int
 vm_fault(int faulttype, vaddr_t faultaddress)
 {
-    (void)faulttype;
     (void)faultaddress;
-    return 0;
+
+    unsigned i;
+    struct addrspace *as;
+
+    int spl;
+    uint32_t ehi, elo;
+    vaddr_t vtop, vbase;
+    paddr_t paddr = 0;
+
+	switch (faulttype) {
+	    case VM_FAULT_READONLY:
+		/* We always create pages read-write, so we can't get this */
+		panic("dumbvm: got VM_FAULT_READONLY\n");
+	    case VM_FAULT_READ:
+	    case VM_FAULT_WRITE:
+		break;
+	    default:
+		return EINVAL;
+	}
+
+    if (curproc == NULL) {
+        return EFAULT;
+    }
+
+    as = proc_getas();
+    if (as == NULL) {
+        return EFAULT;
+    }
+
+    KASSERT(as->as_regions != NULL);
+    
+    for (i = 0; i < as->as_numregions; i++) {
+        struct region *region = as->as_regions[i];
+
+        if (region == NULL) {
+            continue;
+        }
+
+        vbase = region->r_startaddr;
+        vtop = vbase + PAGE_SIZE * region->r_numpages;
+
+        if (faultaddress >= vbase && faultaddress < vtop) {
+            paddr = (faultaddress - vbase) + region->r_pages[0]->vp_paddr;
+        }
+    }
+
+    if (paddr == 0) {
+        kprintf("Page fault!\n");
+        return EFAULT;
+    }
+
+    /* make sure it's page-aligned */
+	KASSERT((paddr & PAGE_FRAME) == paddr);
+
+	/* Disable interrupts on this CPU while frobbing the TLB. */
+	spl = splhigh();
+
+	for (i=0; i<NUM_TLB; i++) {
+		tlb_read(&ehi, &elo, i);
+		if (elo & TLBLO_VALID) {
+			continue;
+		}
+		ehi = faultaddress;
+		elo = paddr | TLBLO_DIRTY | TLBLO_VALID;
+		DEBUG(DB_VM, "dumbvm: 0x%x -> 0x%x\n", faultaddress, paddr);
+		tlb_write(ehi, elo, i);
+		splx(spl);
+		return 0;
+	}
+
+    return EFAULT;
 }
 
 vaddr_t
